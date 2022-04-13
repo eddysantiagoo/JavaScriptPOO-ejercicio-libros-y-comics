3e100059ec25bffc790d9b0c1760c7890fc2061d
eddysantiagoo <eddysantiagogh@gmail.com>
2022-04-13 11:43:52 -0500

    Eliminar varios archivos y agregar JS exclusivo  de esta rama

--- pruebaBranch2.js
+++ pruebaBranch2.js
@@ -1,3 +1,3 @@
-let mensaje = "Esto es una prueba para eliminar algunos archivos y subir un archivo nuevo"
+let mensaje = "Esto es una prueba para eliminar algunos archivos y subir un archivo nuevo "
 
 console.log(mensaje);
